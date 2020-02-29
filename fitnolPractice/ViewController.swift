378deb0ac96e3f931365de85313697704bb5d60c
Muhammad Imran Shah <imran.ssuet178@gmail.com>
2020-02-29 14:07:40 +0500

    Initial Commit

---
+++ fitnolPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitnolPractice
+//
+//  Created by Muhammad Shah on 29/02/2020.
+//  Copyright © 2020 Muhammad Imran Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
