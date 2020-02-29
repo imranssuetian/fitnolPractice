3dab9a7998b45fa4c8c79038b89c902b62dba49e
Muhammad Imran Shah <imran.ssuet178@gmail.com>
2020-02-29 21:39:31 +0500

    Nested Cells, Tabman, Carbon Kit

---
+++ fitnolPractice/Comments/CommentsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentsTableViewCell.swift
+//  fitnolPractice
+//
+//  Created by Muhammad Shah on 29/02/2020.
+//  Copyright © 2020 Muhammad Imran Shah. All rights reserved.
+//
+
+import UIKit
+
+class CommentsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
